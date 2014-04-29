57059cd18afa83f27e4c173b187a580130466f24
Mikola Lysenko <mikolalysenko@gmail.com>
2014-04-28 19:04:24 -0500

    added cwise transform

--- unpack.js
+++ unpack.js
@@ -1,7 +1,18 @@
 "use strict"
 
 var dup = require("dup")
-var do_unpack = require("./dounpack.js")
+var cwise = require("cwise")
+
+var do_unpack = cwise({ 
+  args: ["array", "scalar", "index"],
+  body: function unpackCwise(arr, a, idx) {
+    var v = a, i
+    for(i=0;i<idx.length-1;++i) {
+      v=v[idx[i]]
+    }
+    v[idx[idx.length-1]]=arr
+  }
+})
 
 module.exports = function unpack(arr) {
   var result = dup(arr.shape)
